7af609a8a739e9057a50fcff81785cc280ce0ca3
Mikhail Shabanov <mikhail.shabanov@centigrade.de>
2022-01-10 15:46:01 +0100

    chore: project restructure

--- code/leds.h
+++
@@ -1,8 +0,0 @@
-/* Initializes GPIO*/
-int InitLed(int pin);
-
-/* Bi color LED uses common anode. So, to switch it on we used to set 0 to GPIO pin*/
-void TurnBiColorLedOn(int pin);
-
-/* Bi color LED uses common anode. So, to switch it off we used to set 1 to GPIO pin*/
-void TurnBiColorLedOff(int pin);
