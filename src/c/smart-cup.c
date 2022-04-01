0466e068207e84dff9cc34d03d3f9be9d8ac9193
Dr Mboga <dr.mboga@yahoo.com>
2022-04-01 16:38:41 +0200

    feat: main process start first steps

--- src/c/smart-cup.c
+++ src/c/smart-cup.c
@@ -14,24 +14,27 @@
 
 #define LASER_PIN 4
 
+// If no parapeters - process waits a message for start main process, when main process done, waits another message infintely
+// If parameter "start" - starts main process immediately, finishes the process after maine process done
+// If parameter "tune" - starts tuning program - switch laser and check the sensor
 int main(int argc, char *argv[])
 {
     // Switch Laser off
     int gpioMessageQueueId = InitializeMessageQueue(NATIVE_GPIO_MESSAGE_TYPE);
-    if(gpioMessageQueueId >= 0)
+    if (gpioMessageQueueId >= 0)
     {
         printf("Successfully got GPIO messageQueueId: %d\n", gpioMessageQueueId);
         switchLaserOff(gpioMessageQueueId);
     }
 
-    if(argc > 1)
+    if (argc > 1)
     {
-        if(strcmp(argv[1], "start") == 0)
+        if (strcmp(argv[1], "start") == 0)
         {
             StartMainProcess(gpioMessageQueueId);
             return 0;
         }
-        if(strcmp(argv[1], "tune") == 0)
+        if (strcmp(argv[1], "tune") == 0)
         {
             StartTuningMode(gpioMessageQueueId);
             return 0;
@@ -40,24 +43,24 @@ int main(int argc, char *argv[])
 
     // Initialize Message queue
     int messageQueueId = InitializeMessageQueue(SMART_CUP_CONTROL_MESSAGE_TYPE);
-    
-    if(messageQueueId >= 0)
+
+    if (messageQueueId >= 0)
     {
         printf("Successfully got messageQueueId: %d\n", messageQueueId);
-        while(1) 
+        while (1)
         {
             printf("Waiting for incoming message...\n");
             struct SmartCupProcessStartMsessage received;
             // If no LEDs is blinking, current thread is sleeping while waiting new message from the queue
-            if (msgrcv(messageQueueId, &received, sizeof(received.flag), SMART_CUP_CONTROL_MESSAGE_TYPE, 0)< 0)
+            if (msgrcv(messageQueueId, &received, sizeof(received.flag), SMART_CUP_CONTROL_MESSAGE_TYPE, 0) < 0)
             {
                 PrintMessagingError(errno);
-                if(errno == EINVAL || errno == EIDRM)
+                if (errno == EINVAL || errno == EIDRM)
                 {
                     printf("Re-initializing the queue\n");
                     messageQueueId = InitializeMessageQueue(SMART_CUP_CONTROL_MESSAGE_TYPE);
                     printf("New messageQueueId: %d\n", messageQueueId);
-                    if(messageQueueId == -1)
+                    if (messageQueueId == -1)
                     {
                         return 1;
                     }
@@ -73,17 +76,72 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+#pragma region Main process
+
 void StartMainProcess(int gpioMessageQueueId)
 {
     // Reading cup state from csv file
-    struct CupStateRow* cupState = ReadCupState();
+    struct CupStateRow *cupState = ReadCupState();
     printf("Current pill is %d, state %d, was set up %s\n", cupState->pillNumber, cupState->state, cupState->time);
 
+    while (1)
+    {
+        int nextState = MoveToTheNextState(cupState->state, cupState->pillNumber);
+        printf("Action to perform %d \n", nextState);
+        // TODO:  Perform some action with cupState regarding nextState
+        RewriteCupState(cupState);
+        sleepMilliseconds(500);
+    }
 
-    RewriteCupState(cupState);
     free(cupState);
 }
 
+/*
+ * Desides which action to perform with the cup current state
+ */
+int MoveToTheNextState(int currentState, int pillNumber)
+{
+    switch (currentState)
+    {
+    case CUP_STATE_PILL_INITIAL:
+        printf("Current state is 'initial'. Pill number %d\n", pillNumber);
+        MoveServo(pillNumber);
+        sendCommandToBlinkRedLED(pillNumber);
+        /* Move servo, send signal to blink a LED, check cup. Result - CUP_STATE_PILL_DROPPED_AND_INSIDE or CUP_ERROR_STATE_PILL_DROPPED_BUT_CUP_IS_EMPTY */
+        break;
+    case CUP_STATE_PILL_DROPPED_AND_INSIDE:
+        /* Check if cup is empty. Result - CUP_STATE_PILL_TAKEN or CUP_STATE_PILL_DROPPED_AND_INSIDE */
+        break;
+    case CUP_ERROR_STATE_PILL_DROPPED_BUT_CUP_IS_EMPTY:
+        /* Check cup. Result - CUP_STATE_PILL_DROPPED_AND_INSIDE or CUP_ERROR_STATE_PILL_DROPPED_BUT_CUP_IS_EMPTY */
+        break;
+    default:
+        break;
+    }
+}
+
+/*
+ * Sends command to move servo.
+ */
+void MoveServo(int pillNumber)
+{
+    struct ServoAngleMap *servoMapping = ReadServoMapping();
+    for (int i = 0; i < PILLS_COUNT; i++)
+    {
+        if (servoMapping[i].pillNumber == pillNumber)
+        {
+            sendServoMessage(servoMapping[i].angle);
+            printf("Message to move servo on %d degrees has sent\n", servoMapping[i].angle);
+            sleepMilliseconds(500);
+            break;
+        }
+    }
+
+    free(servoMapping);
+}
+#pragma endregion
+
+#pragma region Tuning mode
 void StartTuningMode(int gpioMessageQueueId)
 {
     int wiringPiHandle = wiringPiI2CSetup(0x23);
@@ -97,32 +155,33 @@ void StartTuningMode(int gpioMessageQueueId)
         for (int i = 0; i < 10; i++)
         {
             // Check the sensor
-            wiringPiI2CWrite(wiringPiHandle,0x10);
+            wiringPiI2CWrite(wiringPiHandle, 0x10);
             sleepMilliseconds(200);
-            int word=wiringPiI2CReadReg16(wiringPiHandle,0x00);
-            int lux=((word & 0xff00)>>8) | ((word & 0x00ff)<<8);
+            int word = wiringPiI2CReadReg16(wiringPiHandle, 0x00);
+            int lux = ((word & 0xff00) >> 8) | ((word & 0x00ff) << 8);
 
-            char* currentTime = nowTimeWithSeconds();
+            char *currentTime = nowTimeWithSeconds();
 
             printf("[%s] Current illuminance in lux:%d \n", currentTime, lux);
             free(currentTime);
             sleepMilliseconds(500);
         }
-        
 
         // Turn Laser off
         switchLaserOff(gpioMessageQueueId);
         sleepMilliseconds(1000);
     }
 }
+#pragma endregion
 
+#pragma region Laser control
 void switchLaserOn(int gpioMessageQueueId)
 {
     struct NativeGpioMsessage gpioMessage;
     gpioMessage.mtype = NATIVE_GPIO_MESSAGE_TYPE;
     gpioMessage.pin = LASER_PIN;
     gpioMessage.action = LED_ON;
-    if(msgsnd(gpioMessageQueueId, (void*)&gpioMessage, sizeof(gpioMessage.pin) + sizeof(gpioMessage.action), 0) < 0)
+    if (msgsnd(gpioMessageQueueId, (void *)&gpioMessage, sizeof(gpioMessage.pin) + sizeof(gpioMessage.action), 0) < 0)
     {
         printf("msgsnd error !!\n");
     }
@@ -133,29 +192,31 @@ void switchLaserOff(int gpioMessageQueueId)
     gpioMessage.mtype = NATIVE_GPIO_MESSAGE_TYPE;
     gpioMessage.pin = LASER_PIN;
     gpioMessage.action = LED_OFF;
-    if(msgsnd(gpioMessageQueueId, (void*)&gpioMessage, sizeof(gpioMessage.pin) + sizeof(gpioMessage.action), 0) < 0)
+    if (msgsnd(gpioMessageQueueId, (void *)&gpioMessage, sizeof(gpioMessage.pin) + sizeof(gpioMessage.action), 0) < 0)
     {
         printf("msgsnd error !!\n");
     }
 }
+#pragma endregion
 
+#pragma region File IO stuff
 /*
-* Reads Cup state from csv file
-*/
-struct CupStateRow* ReadCupState()
+ * Reads Cup state from csv file
+ */
+struct CupStateRow *ReadCupState()
 {
     int pillNumber;
     int state;
     char time[21];
     int rowResult;
 
-    struct CupStateRow* cupState = malloc(sizeof(struct CupStateRow));
+    struct CupStateRow *cupState = malloc(sizeof(struct CupStateRow));
 
-    FILE* cupStateFilePointer = OpenFileForRead(CUP_STATE_FILE_NAME);
+    FILE *cupStateFilePointer = OpenFileForRead(CUP_STATE_FILE_NAME);
 
-    char* todayString = today();
+    char *todayString = today();
 
-    if(cupStateFilePointer == NULL)
+    if (cupStateFilePointer == NULL)
     {
         // File doesn't exist yet
         cupState->pillNumber = 0;
@@ -167,7 +228,7 @@ struct CupStateRow* ReadCupState()
     if ((rowResult = fscanf(cupStateFilePointer, "%d\t%d\t%[^\n]\n", &pillNumber, &state, time)) != EOF)
     {
         printf("Time read: %s\n", time);
-        if(rowResult >= 3)
+        if (rowResult >= 3)
         {
             cupState->pillNumber = pillNumber;
             cupState->state = state;
@@ -182,16 +243,129 @@ struct CupStateRow* ReadCupState()
 }
 
 /*
-* Writes a new Cup state into csv file
-*/
-int RewriteCupState(struct CupStateRow* ledsState)
+ * Writes a new Cup state into csv file
+ */
+int RewriteCupState(struct CupStateRow *ledsState)
 {
-    FILE* cupStateFilePointer = OpenFileForRewrite(CUP_STATE_FILE_NAME);
+    FILE *cupStateFilePointer = OpenFileForRewrite(CUP_STATE_FILE_NAME);
 
-    if(fprintf(cupStateFilePointer, "%d\t%d\t%21s\n", ledsState->pillNumber, ledsState->state, ledsState->time) < 0)
+    if (fprintf(cupStateFilePointer, "%d\t%d\t%21s\n", ledsState->pillNumber, ledsState->state, ledsState->time) < 0)
     {
         printf("Write operaion failed");
         return -1;
     }
     return CloseFile(cupStateFilePointer);
 }
+
+/*
+ * Reads angles mappng from csv file
+ */
+struct ServoAngleMap *ReadServoMapping()
+{
+    int pillNumber;
+    int angle;
+    int rowResult;
+    int pillIndex = 0;
+    struct ServoAngleMap *angles = malloc(PILLS_COUNT * sizeof(struct ServoAngleMap));
+
+    FILE *anglesFilePointer = OpenFileForRead(ANGLES_MAP_FILE_NAME);
+    if (anglesFilePointer == NULL)
+    {
+        // File doesn't exist
+        for (int i = 0; i < PILLS_COUNT; i++)
+        {
+            angles[i].pillNumber = PILLS_COUNT - 1 - i;
+            angles[i].angle = 180;
+        }
+
+        return angles;
+    }
+
+    while ((rowResult = fscanf(anglesFilePointer, "%d\t%d\n", &pillNumber, &angle)) != EOF)
+    {
+        if (rowResult >= 2)
+        {
+            angles[pillIndex].pillNumber = pillNumber;
+            angles[pillIndex].angle = angle;
+            pillIndex++;
+        }
+        if (pillIndex >= PILLS_COUNT)
+        {
+            break;
+        }
+    }
+
+    CloseFile(anglesFilePointer);
+
+    return angles;
+}
+#pragma endregion
+
+#pragma region Servo control
+/*
+ * Sends command to turn servo motor
+ */
+int sendServoMessage(int angle)
+{
+    int messageQueueId = InitializeMessageQueue(SERVO_CONTROL_MESSAGE_TYPE);
+    if (messageQueueId == -1)
+    {
+        return -1;
+    }
+    printf("Successfully got messageQueueId: %d\n", messageQueueId);
+    struct ServoControlMsessage servoMessage;
+    servoMessage.mtype = SERVO_CONTROL_MESSAGE_TYPE;
+    servoMessage.angle = angle;
+
+    if (msgsnd(messageQueueId, (void *)&servoMessage, sizeof(servoMessage.angle), 0) < 0)
+    {
+        printf("msgsnd error !!\n");
+        return -1;
+    }
+
+    return 0;
+}
+#pragma endregion
+
+#pragma region LED control
+/*
+ * Sends command to blink LED red
+ */
+int sendCommandToBlinkRedLED(int ledNumber)
+{
+    return sendLEDMessage(ledNumber, LED_BLINK, RED_LED);
+}
+
+/*
+ * Sends command to turn on LED green
+ */
+int sendCommandToTurnGreenLED(int ledNumber)
+{
+    return sendLEDMessage(ledNumber, LED_ON, GREEN_LED);
+}
+
+int sendLEDMessage(int ledNumber, int action, int color)
+{
+    int messageQueueId = InitializeMessageQueue(LED_CONTROL_MESSAGE_TYPE);
+    if (messageQueueId == -1)
+    {
+        return -1;
+    }
+    printf("Successfully got messageQueueId: %d\n", messageQueueId);
+    struct LedsControlMsessage ledMessage;
+    ledMessage.mtype = LED_CONTROL_MESSAGE_TYPE;
+    ledMessage.LedNumber = ledNumber;
+    ledMessage.LedAction = action;
+    ledMessage.LedColor = color;
+
+    if (msgsnd(messageQueueId, (void *)&ledMessage, sizeof(ledMessage.LedAction) + sizeof(ledMessage.LedNumber) + sizeof(ledMessage.LedColor), 0) < 0)
+    {
+        printf("msgsnd error !!\n");
+        return -1;
+    }
+
+    // msgctl(messageQueueId, IPC_RMID, NULL);
+    return 0;
+}
+
+#pragma endregion
--- src/c/smart-cup.h
+++ src/c/smart-cup.h
@@ -1,20 +1,32 @@
-static const char CUP_STATE_FILE_NAME[] = "cup-state.csv"; // File name with the LEDs state
+static const char CUP_STATE_FILE_NAME[] = "cup-state.csv"; // File name with the cup state
+static const char ANGLES_MAP_FILE_NAME[] = "angles.csv";   // File name with the servo angles mapping
 
-#define CUP_STATE_PILL_INITIAL 0 // Signal to move servo has sent and signal to blink the LED has sent
-#define CUP_STATE_PILL_DROPPED 1 // Signal to move servo has sent and signal to blink the LED has sent
-#define CUP_STATE_PILL_DROPPED_AND_INSIDE 2 // After moving servo, the pill should be in the cup
+#define PILLS_COUNT 7
+
+#define CUP_STATE_PILL_INITIAL 0                         // No actions have been made yet
+#define CUP_STATE_PILL_DROPPED 1                         // Signal to move servo has sent and signal to blink the LED has sent
+#define CUP_STATE_PILL_DROPPED_AND_INSIDE 2              // After moving servo, the pill should be in the cup
 #define CUP_ERROR_STATE_PILL_DROPPED_BUT_CUP_IS_EMPTY -3 // Error state - the servo has moved but the pill has stuck somewhere
-#define CUP_STATE_PILL_TAKEN 4 // Pill has taken, signal to turn green LED has sent, the row has added to the journal
-#define CUP_ERROR_STATE_PILL_NOT_TAKEN -5 // Error state - at 20:00 pill is still in the cup, the signal to turn red LED has sent, the row has added to the journal 
+#define CUP_STATE_PILL_TAKEN 4                           // Pill has taken, signal to turn green LED has sent, the row has added to the journal
+#define CUP_ERROR_STATE_PILL_NOT_TAKEN -5                // Error state - at 20:00 pill is still in the cup, the signal to turn red LED has sent, the row has added to the journal
 
 /*
-* Csv smart cup state file data row structure
-*/
+ * Csv smart cup state file data row structure
+ */
 struct CupStateRow
 {
     int pillNumber;
     int state;
     char time[21];
+};
+
+/*
+ * Csv with mapping the pill number with servo angle
+ */
+struct ServoAngleMap
+{
+    int pillNumber;
+    int angle;
 };
 
 void StartMainProcess(int gpioMessageQueueId);
@@ -24,11 +36,43 @@ void switchLaserOn(int gpioMessageQueueId);
 void switchLaserOff(int gpioMessageQueueId);
 
 /*
-* Reads Cup state from csv file
-*/
-struct CupStateRow* ReadCupState();
+ * Reads Cup state from csv file
+ */
+struct CupStateRow *ReadCupState();
 
 /*
-* Writes a new Cup state into csv file
-*/
-int RewriteCupState(struct CupStateRow* ledsState);
+ * Writes a new Cup state into csv file
+ */
+int RewriteCupState(struct CupStateRow *ledsState);
+
+/*
+ * Desides which action to perform with the cup current state
+ */
+int MoveToTheNextState(int currentState, int pillNumber);
+
+/*
+ * Sends command to move servo.
+ */
+void MoveServo(int pillNumber);
+
+/*
+ * Reads angles mappng from csv file
+ */
+struct ServoAngleMap *ReadServoMapping();
+
+/*
+ * Sends command to turn servo motor
+ */
+int sendServoMessage(int angle);
+
+/*
+ * Sends command to blink LED red
+ */
+int sendCommandToBlinkRedLED(int ledNumber);
+
+/*
+ * Sends command to turn on LED green
+ */
+int sendCommandToTurnGreenLED(int ledNumber);
+
+int sendLEDMessage(int ledNumber, int action, int color);
